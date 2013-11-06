87c59aabc4f1113f0f07348a8888c762fa491089
Amanda Neumann <amandaneumann@me.com>
2013-11-06 16:28:16 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+== Amanda Neumann - Personal Website
+
+This is a rails 4.0 application built as a personal website.
+The purpose of this website is to include a simple blog along with some background and links around my work history.
+
+View the site at www.amandaneumann.com
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amandaneumann::Application.config.secret_token = '0ca57204279ff2ecbfacbb755e398e6ecdbea297907f533902b8dae62ee71f2c05c48da7beeb0e8e76a554c3e5589d3222aa723d4b831609572c8972850bba6e'
